71420367597761e92663303def23260f7ebfbd15
Joshua <Joshua@host.docker.internal>
2023-04-19 19:43:09 +0100

    fixxed smelly code

--- src/main/java/com/tus/charactersheet/model/Character.java
+++ src/main/java/com/tus/charactersheet/model/Character.java
@@ -2,10 +2,12 @@ package com.tus.charactersheet.model;
 
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Character {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
